ede9bb4ce1e3f056145a64e1ac96fb3370c66a74
gustaveH <ghabiman@gmail.com>
2020-08-18 12:55:29 -0400

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import image from '../images/queen.jpg';
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>landing page</h1>
+      <div className='home-page'>
+        <img src={image} alt='home' />
+        <h1>Welcome To Keza Queens</h1>
       </div>
     );
   }
