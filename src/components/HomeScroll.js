26a89d051cb2cd14e0c801a1e553e13f3bd5c196
gustaveH <ghabiman@gmail.com>
2020-08-24 11:54:20 -0400

    deploy

--- src/components/HomeScroll.js
+++ src/components/HomeScroll.js
@@ -1,15 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../App.css';
-import { Link } from 'react-scroll';
-
 export default function HomeScroll({ scrollTitle }) {
-  const [click, setClick] = useState(false);
+  // const [click, setClick] = useState(false);
 
-  function welcomePage() {
-    console.log('hello');
-  }
+  // function welcomePage() {
+  //   console.log('hello');
+  // }
   return (
-    <div className='arrow-down' onClick={welcomePage}>
+    <div className='arrow-down'>
       <p className='scroll-button'>{scrollTitle}</p>
     </div>
   );
