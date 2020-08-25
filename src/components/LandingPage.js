4c7b09ca287ff63f3c81986351c1304044140219
gustaveH <ghabiman@gmail.com>
2020-08-25 16:44:51 -0400

    navbar

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import image from '../images/queen.jpg';
+
 import '../App.css';
 import Banner from '../components/Banner';
 // import SocialMedia from './SocialMedia';
@@ -8,7 +8,6 @@ function LandingPage() {
   return (
     <>
       <div className='home-page'>
-        <img src={image} alt='home' />
         <div className='welcome-message'>
           <Banner title='Welcome to keza queens' />
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,55 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 import { Link } from 'react-router-dom';
-import { FaBars, FaTimes } from 'react-icons/fa';
+import { FaBars, FaTimes, FaCrown } from 'react-icons/fa';
 
-export default class Navbar extends Component {
-  state = {
-    clicked: false,
-    scroll: false,
-  };
-  handleClick = () => {
-    this.setState({ clicked: !this.state.clicked });
-  };
+export default function Navbar() {
+  const [click, setClick] = useState(false);
+  const [close, setClose] = useState(false);
 
-  closeNav = () => {
-    this.setState({
-      clicked: false,
-    });
-  };
-
-  render() {
-    return (
-      <nav className='navbar'>
-        <Link to='/' onClick={this.closeNav}>
-          <h1 className='navbar-logo'>Keza Queens</h1>
+  const handleClick = () => setClick(!click);
+  const closeMenu = () => setClose(!click);
+  return (
+    <nav className='navbar'>
+      <div className='navbar-container'>
+        <Link to='/'>
+          <h1 className='navbar-logo'>
+            Keza Queens <FaCrown className='crown' />
+          </h1>
         </Link>
-        <div className='menu-icon' onClick={this.handleClick}>
-          {this.state.clicked ? <FaTimes /> : <FaBars />}
+        <div className='menu-icon' onClick={handleClick}>
+          {click ? <FaTimes /> : <FaBars />}
         </div>
-        <ul className={this.state.clicked ? 'nav-links active' : 'nav-links'}>
-          <li>
-            <Link to='/About' onClick={this.closeNav}>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className='nav-item'>
+            <Link to='/About' className='nav-links' onClick={closeMenu}>
               About
             </Link>
           </li>
-          <li>
-            <Link to='/Podcast' onClick={this.closeNav}>
+          <li className='nav-item'>
+            <Link to='/Podcast' className='nav-links' onClick={closeMenu}>
               Podcast
             </Link>
           </li>
-          <li>
-            <Link to='/Chat' onClick={this.closeNav}>
+          <li className='nav-item'>
+            <Link to='/Chat' className='nav-links' onClick={closeMenu}>
               Chat
             </Link>
           </li>
-          <li>
-            <Link to='/Admin' onClick={this.closeNav}>
+          <li className='nav-item'>
+            <Link to='/Admin' className='nav-links' onClick={closeMenu}>
               Admin
             </Link>
           </li>
         </ul>
-      </nav>
-    );
-  }
+      </div>
+    </nav>
+  );
 }
