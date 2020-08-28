b5edcf6baef708a55d8a9f6871880d888552f605
gustaveH <ghabiman@gmail.com>
2020-08-28 12:30:34 -0400

    style

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -12,56 +12,6 @@ function LandingPage() {
         </div>
         <SocialMedia className='social' />
       </div>
-      <div>
-        <p>
-          Sit duis cupidatat fugiat pariatur. Labore fugiat laboris est culpa
-          magna eu eiusmod dolore incididunt. Eiusmod ut aliqua occaecat sunt
-          mollit qui magna exercitation nisi eiusmod ipsum voluptate nulla est.
-          Pariatur duis ea labore est. Velit elit aliquip velit exercitation id
-          proident minim velit. Reprehenderit labore in laboris irure laboris
-          dolor ad. In dolore ea laborum ullamco commodo magna anim. Sit cillum
-          ullamco id Lorem aliqua ut nisi veniam veniam. Eiusmod ullamco
-          pariatur ipsum cupidatat mollit do. Reprehenderit labore irure anim
-          irure officia deserunt proident. Excepteur sit eiusmod id elit
-          consectetur nisi in commodo. Sunt laborum deserunt do mollit
-          incididunt exercitation qui aliquip. Proident pariatur minim
-          consectetur adipisicing officia mollit occaecat ex laborum ex amet
-          dolor sit elit. Nostrud sunt deserunt laboris id. Laboris enim in
-          dolor fugiat Lorem. Duis ad reprehenderit est laboris culpa. Ipsum
-          excepteur qui ullamco non do duis. Consectetur incididunt aute tempor
-          excepteur cillum. Dolore ad sit mollit dolore officia. In quis do
-          dolore laboris eu consequat cupidatat nostrud esse. Culpa ipsum
-          consectetur ullamco proident cupidatat consectetur ullamco elit tempor
-          tempor est. Minim Lorem ea ex ad ea mollit amet incididunt excepteur.
-          Elit velit anim ullamco consectetur nisi mollit amet magna. Proident
-          do velit minim dolore fugiat in veniam amet nisi labore velit in.
-          Pariatur nostrud aute minim in eu proident laboris excepteur velit
-          culpa dolore do. Exercitation adipisicing deserunt cupidatat labore.
-          Occaecat reprehenderit sit fugiat quis et minim cillum veniam nisi
-          velit. Quis eiusmod fugiat occaecat aliquip exercitation id mollit ea
-          est occaecat exercitation. Esse velit aliquip velit velit culpa id
-          tempor pariatur fugiat commodo. Pariatur esse culpa occaecat consequat
-          pariatur. Laboris laborum velit nulla consectetur aliquip commodo et
-          occaecat ipsum aute exercitation do consequat dolor. Aute voluptate
-          exercitation velit aliquip minim. Sit est exercitation proident dolore
-          ad fugiat mollit aute dolore in consectetur aliqua. Sit laboris sunt
-          veniam minim. Nostrud ad sint est eu cupidatat voluptate cillum cillum
-          eiusmod consequat dolore consequat. Cillum velit non deserunt sunt
-          amet enim qui do mollit. Nulla eu culpa occaecat irure nulla mollit
-          eiusmod quis. Quis ut ut exercitation mollit proident consequat
-          adipisicing ad aute est anim. Veniam aliquip nostrud reprehenderit est
-          aliqua. Cillum eiusmod aute culpa non nulla tempor laborum ullamco
-          exercitation ea do incididunt ex elit. Consequat reprehenderit
-          adipisicing ea culpa excepteur anim tempor. Magna reprehenderit ad
-          nulla mollit pariatur in ad. Magna commodo non ut officia fugiat sit
-          pariatur ipsum cillum amet. Consectetur est qui cillum enim amet
-          proident esse mollit ea eu proident est. Consectetur cupidatat nulla
-          excepteur dolore cupidatat ea nostrud voluptate veniam ex ex. Duis
-          anim exercitation amet irure Lorem mollit fugiat qui. Eu sint sit
-          occaecat tempor consectetur velit magna ipsum commodo laboris deserunt
-          labore. Laborum ut id dolor magna aliqua quis consectetur nostrud.
-        </p>
-      </div>
     </>
   );
 }
