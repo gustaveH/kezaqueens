7dbf74563866223355ce1d74a0bf5f82c309421b
gustaveH <ghabiman@gmail.com>
2020-09-01 15:58:18 -0400

    stylig

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -15,7 +15,16 @@ export default function Profile() {
             name='Martina Usanase'
             bio='Irure sunt et esse ea ad amet ea. 
             Voluptate deserunt est qui aliquip Lorem ut 
-            dolore aliqua tempor ea ullamco enim.'
+            dolore aliqua tempor ea ullamco enim.Pariatur 
+            est mollit duis pariatur in ullamco eu pariatur
+            laboris ullamco qui. Non esse veniam nostrud est 
+            eiusmod cupidatat nisi occaecat labore consectetur
+            amet dolore ullamco ex. Consectetur consequat id 
+            tempor adipisicing nulla tempor. Reprehenderit ea
+            non mollit deserunt adipisicing velit magna velit labore et.
+            Adipisicing commodo cillum occaecat ad ea mollit.
+            Commodo deserunt aliquip Lorem cupidatat aliquip 
+            cupidatat dolor ut eu magna est. '
             social={<FaInstagram />}
           />
         </div>
@@ -25,7 +34,16 @@ export default function Profile() {
             name='Grace Karera'
             bio='Irure sunt et esse ea ad amet ea. 
             Voluptate deserunt est qui aliquip Lorem ut 
-            dolore aliqua tempor ea ullamco enim.'
+            dolore aliqua tempor ea ullamco enim.Pariatur 
+            est mollit duis pariatur in ullamco eu pariatur
+            laboris ullamco qui. Non esse veniam nostrud est 
+            eiusmod cupidatat nisi occaecat labore consectetur
+            amet dolore ullamco ex. Consectetur consequat id 
+            tempor adipisicing nulla tempor. Reprehenderit ea
+            non mollit deserunt adipisicing velit magna velit labore et.
+            Adipisicing commodo cillum occaecat ad ea mollit.
+            Commodo deserunt aliquip Lorem cupidatat aliquip 
+            cupidatat dolor ut eu magna est. '
             social={<FaInstagram />}
           />
         </div>
