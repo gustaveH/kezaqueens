6240831b771cd9457134b5dfd71e7556d33a0351
gustaveH <ghabiman@gmail.com>
2020-09-04 13:18:04 -0400

    podcast styling and show info

--- src/components/AdminForm.js
+++ src/components/AdminForm.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import '../App.css';
 
 export default class AdminForm extends Component {
   render() {
-    return <div></div>;
+    return (
+      <section className='wrapper'>
+        <Form className='adminForm'>
+          <Form.Group controlId='formBasicEmail'>
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type='email' placeholder='Enter email' />
+            <Form.Text className='text-muted'>
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId='formBasicPassword'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control type='password' placeholder='Password' />
+          </Form.Group>
+          <Button variant='primary' type='submit'>
+            Submit
+          </Button>
+        </Form>
+      </section>
+    );
   }
 }
--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import AdminForm from '../components/AdminForm';
 
 export default class Admin extends Component {
   render() {
     return (
-      <div>
-        <h1>admin page</h1>
+      <div className='admin'>
+        <AdminForm />
       </div>
     );
   }
