1fffa6e019ee536ae728ea8ea28a8d4b0cec3fe7
gustaveH <ghabiman@gmail.com>
2020-09-04 12:12:33 -0400

    podcast styling and show info

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
-import image from '../images/podcast.jpg';
 import { FaCaretSquareDown } from 'react-icons/fa';
+import { SiApplepodcasts, SiSpotify } from 'react-icons/si';
 import '../App.css';
 export default class SingleCard extends Component {
+  state = {
+    showInfo: false,
+  };
+  handleInfo = () => {
+    this.setState({
+      showInfo: !this.state.showInfo,
+    });
+  };
   render() {
+    const { title, podcast, img, info } = this.props.podcast;
     return (
-      <article className='podcast'>
+      <article className='singleCard'>
         <div className='image-container'>
-          <img width='200' src={image} alt='podcast' />
+          <img src={img} alt=' ' />
+          <span className='date'>
+            <p>Date here</p>
+          </span>
         </div>
         <div className='podcast-info'>
-          <h3>Title</h3>
-          <h4>Episode, date</h4>
+          <h3>{title}</h3>
+          <h4>{podcast}</h4>
           <h5>
             Info{' '}
-            <span>
+            <span onClick={this.handleInfo}>
               <FaCaretSquareDown className='info-dropdown' />
             </span>
           </h5>
-          <p>
-            Elit pariatur irure cillum enim irure. Sint excepteur pariatur do
-            aliqua duis mollit ea tempor.
-          </p>
+          {this.state.showInfo && <p>{info}</p>}
+
+          <span className='pod-social'>
+            <SiApplepodcasts className='itunes' />
+            <SiSpotify className='spotify' />
+          </span>
         </div>
       </article>
     );
--- src/pages/Podcast.js
+++ src/pages/Podcast.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import SingleCard from '../components/SingleCard';
+import { Data } from '../Data';
+import '../App.css';
 
 export default class Podcast extends Component {
+  state = {
+    podcasts: Data,
+  };
   render() {
+    const { podcasts } = this.state;
     return (
-      <div>
-        <SingleCard />
-      </div>
+      <section className='podcast'>
+        {podcasts.map((podcast) => {
+          return <SingleCard key={podcast.id} podcast={podcast} />;
+        })}
+      </section>
     );
   }
 }
